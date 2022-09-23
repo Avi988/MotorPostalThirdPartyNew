93a28d9aefd87e02eb6857b04010095e591626e0
Anshumal Kumarage <anshuwa@gmail.com>
2022-09-23 16:45:45 +0530

    Sample .DTO

---
+++ MotorPostalThirdParty/DTO/ThirdPartyCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotorPostalThirdParty.DTO
+{
+    public class ThirdPartyCard
+    {
+        public string PolicyNumber { get; set; }
+        public string VehicleNumber { get; set; }
+        public string NICNumber { get; set; }
+
+        // Similarly all the properties of the card
+    }
+}
