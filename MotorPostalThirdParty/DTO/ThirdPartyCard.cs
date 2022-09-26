a165654e342696f8e49a104b32445d88af4620d3
Avishka <avishkadevangi@gmail.com>
2022-09-26 09:25:00 +0530

    Propetrites Card

--- MotorPostalThirdParty/DTO/ThirdPartyCard.cs
+++ MotorPostalThirdParty/DTO/ThirdPartyCard.cs
@@ -10,6 +10,13 @@ namespace MotorPostalThirdParty.DTO
         public string PolicyNumber { get; set; }
         public string VehicleNumber { get; set; }
         public string NICNumber { get; set; }
+        public string ChassisNo { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PeriodOfCover { get; set; }
+        public string Place { get; set; }
+        public string RefNo { get; set; }
+        public double RsValue { get; set; }
 
         // Similarly all the properties of the card
     }
