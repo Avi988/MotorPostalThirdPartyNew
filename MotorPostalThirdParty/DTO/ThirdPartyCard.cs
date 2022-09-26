ada1c589ff26efb1ec88cf4388e10fb69d2793b4
Avishka <avishkadevangi@gmail.com>
2022-09-26 10:12:45 +0530

    Last Change

--- MotorPostalThirdParty/DTO/ThirdPartyCard.cs
+++ MotorPostalThirdParty/DTO/ThirdPartyCard.cs
@@ -12,13 +12,30 @@ namespace MotorPostalThirdParty.DTO
         public string NICNumber { get; set; }
         public string ChassisNo { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
         public string PeriodOfCover { get; set; }
         public string Place { get; set; }
         public string RefNo { get; set; }
         public double RsValue { get; set; }
+        public string SN { get; set; }
+        public string SNDate { get; set; }
+        public string CoverList { get; set; }
 
         
+        ThirdPartyCard cd = new ThirdPartyCard();
+
+        public ThirdPartyCard()
+        {
+
+        }
+
+        //public ThirdPartyCard(string PolicyNumber,string VehicleNumber,string NICNumber,string ChassisNo,string Name,string Address,string )
+        //{
+            
+        //}
+
+
 
         // Similarly all the properties of the card
     }
